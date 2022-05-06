0f64e7a41657b28e590c37e7651c75b7e5ca8426
JoanUniverse <jonnyy184@gmail.com>
2022-05-06 18:09:21 +0200

    Added username return after login success

--- SpotTheMusic_API/app/Http/Controllers/LoginController.php
+++ SpotTheMusic_API/app/Http/Controllers/LoginController.php
@@ -14,7 +14,7 @@ class LoginController extends Controller
     {
         $user = User::where('email', $request->input('email'))->first();
         if ($user && Hash::check($request->input('password'), $user->password)) {
-            return response()->json(['status' => 1, 'message' => 'Status OK',  'id_user' => $user['id_user']]);
+            return response()->json(['status' => 1, 'message' => 'Status OK',  'id_user' => $user['id_user'], 'username' => $user['username']]);
         } else {
             return response()->json(['status' => 0, 'message' => 'fail'], 401);
         }
