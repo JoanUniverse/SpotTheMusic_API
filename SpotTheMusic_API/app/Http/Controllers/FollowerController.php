7964e87a0c2dbb80736c35c8001b4e8870b2dab9
JoanUniverse <jonnyy184@gmail.com>
2022-05-18 21:54:32 +0200

    Now follower endpoints return User objects

--- SpotTheMusic_API/app/Http/Controllers/FollowerController.php
+++ SpotTheMusic_API/app/Http/Controllers/FollowerController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Follower;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class FollowerController extends Controller
@@ -16,14 +17,16 @@ class FollowerController extends Controller
     //Return all followers from 1 user
     public function show($id)
     {
-        $followers = Follower::where("userFollowed", "=", $id)->get();
+        $followers = User::join('followers', 'followers.userFollows', '=', 'users.id_user')
+                            ->where("followers.userFollowed", "=", $id)->get();
         return response()->json($followers);
     }
 
     //Return all users that 1 user follows
     public function showFollows($id)
     {
-        $followers = Follower::where("userFollows", "=", $id)->get();
+        $followers = User::join('followers', 'followers.userFollowed', '=', 'users.id_user')
+                ->where("userFollows", "=", $id)->get();
         return response()->json($followers);
     }
 
@@ -36,7 +39,8 @@ class FollowerController extends Controller
 
     //Return users that I don't follow
     public function showNewFollowers($id){
-        $followers = Follower::where("userFollows", "!=", $id)->where("userFollowed", "!=", $id)->get();
+        $followers = User::whereNotIn('id_user', Follower::where("userFollows", "=", $id)->select('userFollowed'))
+                            ->where("id_user", "!=", $id)->get();
         return response()->json($followers);
     }
 
