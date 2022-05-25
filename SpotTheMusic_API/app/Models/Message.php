8fc124010ae5501c2114ce607213b6e0ff88758e
JoanUniverse <jonnyy184@gmail.com>
2022-05-25 19:53:57 +0200

    Info user in messages

--- SpotTheMusic_API/app/Models/Message.php
+++ SpotTheMusic_API/app/Models/Message.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\UserShort;
+
 
 class Message extends Model
 {
@@ -12,4 +14,15 @@ class Message extends Model
     protected $table = 'messages';
     protected $primaryKey = 'id_message';
     public $timestamps = false;
+    public static $snakeAttributes = false;
+
+    protected $with=['userFrom', 'userTo'];
+
+    public function userFrom() {
+        return $this->belongsTo(UserShort::class,'userFrom','id_user');
+    }
+
+    public function userTo() {
+        return $this->belongsTo(UserShort::class,'userTo','id_user');
+    }
 }
