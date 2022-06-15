7c8ae50a7102c624b41a769253f100dbd8e5f40a
JoanUniverse <jonnyy184@gmail.com>
2022-06-15 13:32:23 +0200

    FIX

--- SpotTheMusic_API/app/Http/Controllers/UserController.php
+++ SpotTheMusic_API/app/Http/Controllers/UserController.php
@@ -122,7 +122,8 @@ class UserController extends Controller
     public function nearUsers(Request $request, $id)
     {
         $value = $request->distance;
-        if($value === 3) {
+
+        if($value == 3) {
             $user = User::findOrFail($id);
             $location = $user->location;
             if (!$user->allowLocation) return response()->json(['status' => 'This user has not allowed location term'], 403);
@@ -130,6 +131,7 @@ class UserController extends Controller
             $users = User::where("location", "!=", null)->where("allowLocation", "!=", 0)->where("id_user", "!=", $id)->get();
             return response()->json(['status' => 1, 'result' => $users]);
         }
+
         if($value > 2 || $value < 0) return response()->json(['status' => 'Range value must be -> 0, 1 or 2'], 403);
 
         $range = array(0.2, 0.5, 1.0);
